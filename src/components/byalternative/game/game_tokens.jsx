9ce15a65fc7098db21dcef4490e4fdff57c9171b
JeronimoHoulin <jeronimo.houlin@gmail.com>
2022-04-06 11:08:43 -0300

    finished what was on dev

--- src/components/byalternative/game/game_tokens.jsx
+++ src/components/byalternative/game/game_tokens.jsx
@@ -6,13 +6,43 @@ export default function Games() {
 
   const { tokens } = useAPI_alt();
 
-  //console.log(tokens);
+  console.log(tokens);
+
+  let game_tokens = []
+
+  const token_300 = tokens.slice(-500);
+
+  for(let i=0; i<token_300.length; i++){
+
+      //////////////////////////////////////////////////
+      const regex_game = [/utili/, /game/, /p2e/, /play/, /p2e/];
+      const tags_game = token_300[i].tags;
+      //let matchin = regex_game.some(rx => rx.test(tags));
+      //console.log(matchin);
+  
+      if(regex_game.some(rx => rx.test(tags_game))===true && token_300[i].extensions ){
+          
+          game_tokens.push({
+              ID:i,
+              Address:token_300[i].address,
+              Name: token_300[i].name,
+              Symbol: token_300[i].symbol,
+              Tags: token_300[i].tags,
+              logo: token_300[i].logoURI,
+              Extensions:token_300[i].extensions,
+              Timestamp: "Loading...",
+              Category: "Game"
+            })
+      }
+
+  }
 
   return (
-    <ul>
-      {tokens.map(item => (
-        <li>{item.address}</li>
+    <div>
+      <h1>Hello Alternative Game token community!</h1>
+      {game_tokens.map(item => (
+        <p>Name: {item.Name} // Symbol: {item.Symbol} === Get ALT data</p>
       ))}
-    </ul>
+    </div>
   );
 }
--- src/components/bymarket/cat1/cat1.jsx
+++ src/components/bymarket/cat1/cat1.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import { useAPI_mrkt } from "../mrkt_fetcher";
 
 export default function Cat1() {
 
-    
+  const [category, setCategory] = useState([]);
+
   const { categories } = useAPI_mrkt();
 
-  console.log(categories);
+  //console.log(categories);
+
+
+  async function fetchCategory(){
+    ////////////////////////CATEGORY
+    const { data } = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=gaming&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+
+    );
+    //console.log(data);
+    setCategory(data);
+  }
+
+  fetchCategory();
+
+
 
   return (
     <ul>
-      {categories.map(item => (
+      <p>All categories are: {categories.length}</p>
+      <p>Gaming category:</p>
+      {category.map(item => (
         <p>{item.name +".\n" + "Mark Cap: " + item.market_cap.toLocaleString('en-US')}</p>
         ))}
     </ul>
